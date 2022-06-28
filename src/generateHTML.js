f5ac6ca73b9226505ea1643e8b43f8cf5ac1f0f2
David Ferris <95839065+dferris1135@users.noreply.github.com>
2022-06-27 21:03:41 -0700

    updated generateHTML.js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,7 +1,6 @@
-// create the team
+
 const generateTeam = team => {
 
-    // create the manager html
     const generateManager = manager => {
         return `
         <div class="card employee-card">
@@ -20,7 +19,7 @@ const generateTeam = team => {
         `;
     };
 
-    // create the html for engineers
+
     const generateEngineer = engineer => {
         return `
         <div class="card employee-card">
@@ -39,7 +38,7 @@ const generateTeam = team => {
         `;
     };
 
-    // create the html for interns
+
     const generateIntern = intern => {
         return `
         <div class="card employee-card">
@@ -84,30 +83,26 @@ module.exports = team => {
 
     return `
     <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>My Team</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    <script src="https://kit.fontawesome.com/c502137733.js"></script>
-</head>
-
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading">
-                <h1 class="text-center">My Team</h1>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team App</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 jumbotron mb-3 team-members">
+                    <h1 class="text-center">Team Members</h1>
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="team-area col-12 d-flex justify-content-center">
+            <div class="container">
+                <div class="row">
+                    <div class="team-area col-12 d-flex justify-content-center">
                 ${generateTeam(team)}
             </div>
         </div>
